0febd27f185a2a2a692a0aad8171cb2d3362b80e
Milan <milanvpage@gmail.com>
2021-09-27 13:34:23 -0700

    commit tree d15be0c1fb179a2e88cb989918fa8b2a8b10ff3b

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
-}
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  };
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '');
+  }
+
+  static titleize(string) {
+    let exception = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let arrOfWords = string.split(" ")
+    let titleizedArr = arrOfWords.map(word => {
+      if (exception.includes(word) && word !== arrOfWords[0]) {
+        return word 
+      } else {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      }
+    }); 
+    return titleizedArr.join(" ");
+  }
+
+
+};
